4b4718cf91371025cc56cf57b37d28a07f2c7fdc
pedrocaazev <pedroazevedoios1@gmail.com>
2025-12-04 08:41:28 -0300

    Camada de visão feita

---
+++ view/lanche_cadastrar.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Scat :: Cadastrar</title>
+    <link rel="stylesheet" href="view/style.css"> 
+
+    
+    <body>
+    <main>
+        <form action="" method="post">
+
+        <label for="txtNome">Nome do Lanche</label>
+        <input type="text" name="txtNome" id="txtNome" placeholder="Escreva o nome do lanche"> 
+        
+
+        <label for="txtIngredientes">Ingredientes</label>
+        <input type="text" name="txtIngredientese" id="txtIngredientes" > 
+
+        <label for="txtPreco">Preço do Lanche</label>
+        <input type="text" name="txtPreco" id="txtPreco" > 
+
+        <button type="submit">Cadastrar</button>
+
+        </form>
+    </main>
+    
+</body>
+</html>
