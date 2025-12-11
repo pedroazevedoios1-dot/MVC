6893361d05cae0e10ff16dfee3a988b21ed35c54
pedrocaazev <pedroazevedoios1@gmail.com>
2025-12-11 08:31:27 -0300

    Classe Lanche

---
+++ model/lanche.php
@@ -0,0 +1,73 @@
+<?php
+class Lanche
+{
+    // Atributos
+    private $nome;
+    private $ingredientes;
+    private $preco;
+
+/**
+     * Set the value of nome
+     *
+     * @return  self
+     */ 
+    public function setNome($nome)
+    {
+        $this->nome = $nome;
+
+        return $this;
+    }
+
+    
+    /**
+     * Get the value of nome
+     */ 
+    public function getNome()
+    {
+        return $this->nome;
+    }
+
+
+    /**
+     * Get the value of ingredientes
+     */ 
+    public function getIngredientes()
+    {
+        return $this->ingredientes;
+    }
+
+    /**
+     * Set the value of ingredientes
+     *
+     * @return  self
+     */ 
+    public function setIngredientes($ingredientes)
+    {
+        $this->ingredientes = $ingredientes;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of preco
+     */ 
+    public function getPreco()
+    {
+        return $this->preco;
+    }
+
+    /**
+     * Set the value of preco
+     *
+     * @return  self
+     */ 
+    public function setPreco($preco)
+    {
+        $this->preco = $preco;
+
+        return $this;
+    }
+
+
+}
+
